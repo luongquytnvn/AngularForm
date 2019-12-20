bb3cb9c4da753669f5a869c01f0f028c6e9eca43
luongquytnvn <luongquytnvn@gmail.com>
2019-12-20 08:36:35 +0700

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterFinalComponent } from './register-final/register-final.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    RegisterComponent,
+    LoginComponent,
+    RegisterFinalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
